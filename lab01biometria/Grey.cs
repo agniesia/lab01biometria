36d52a11d5d5c13db62412e5adcc47ddaf979e20
Agnieszka <agnieszka_szczotka@onet.pl>
2014-03-18 17:26:02 +0100

    ale

---
+++ lab01biometria/Grey.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01biometria
+{
+    class image_Gray : image_as_tab
+    {
+        byte[][] Greycanal;
+        public image_Gray() : base() { }
+
+        public image_Gray(byte[] orginal_tab, int wight, int hight)
+            : base(orginal_tab, wight, hight)
+        {
+
+            Greycanal = new byte[wight][];
+            var k = 0;
+            for (int i = 0; i < wight; i++)
+            {
+                Greycanal[i] = new byte[hight];
+                for (int j = 0; j < hight; j++)
+                {
+                    k = 4 * (j * w + i);
+                    Greycanal[i][j] = orginal_tab[k];
+                }
+            }
+        }
+        public void normalize()
+        {
+            //max
+            var max = utab.Max();
+
+            //min
+            var min = utab.Min();
+
+            var normalizeConst = max - min;
+
+
+
+            ///pixel[x,y]=255*(pixel[x,y]-minPix)/(maxPix-minPix) 
+            //[] utab1=  utab.Select((x,i )=> i%4!=0 ).ToArray().Select(x => 255 * (utab[ - min) / normalizeConst).ToArray();
+            for (int i = 0; i < utab.Length; i++)
+            {
+                if (i % 4 != 0)
+                {
+                    utab[i] = (byte)(255 * (utab[i] - min) / normalizeConst);
+                }
+
+            }
+
+
+
+
+
+
+        }
+        public image_RGB histogram()
+        {
+            int[] Hist = new int[256];
+            for (int k = 0; k < 256; k++)
+            {
+                Hist[k] = utab.Where((x, i) => i % 4 == 0 && x == k).ToArray().Length;
+            }
+
+            int max = Hist.Max();
+            for (int i = 0; i < 256; i++)
+            {
+                Hist[i] = 180 * Hist[i] / max;
+            }
+
+            byte[][] histobraz = new byte[256][];
+            for (int i = 0; i < 256; i++)
+            {
+                histobraz[i] = new byte[180];
+
+                for (int j = 0; j < 180; j++)
+                {
+                    if (j < (180 - Hist[i]))
+                        histobraz[i][j] = 0;
+                    else
+                        histobraz[i][j] = 100;
+                }
+            }
+            byte[] temp = new byte[256 * 180 * 4];
+
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 180; j++)
+                {
+
+                    temp[4 * (j * 256 + i)] = histobraz[i][j];
+                    temp[4 * (j * 256 + i) + 1] = histobraz[i][j];
+                    temp[4 * (j * 256 + i) + 2] = histobraz[i][j];
+                    temp[4 * (j * 256 + i) + 3] = 255;
+
+
+                }
+            }
+
+            return new image_RGB(temp, 256, 180);
+        }
+        public void BinaryGlobalMean()
+        {
+            var mean = utab.Sum(x => x) / utab.Length;
+            byte one = 1;
+            byte zero = 0;
+            utab = utab.Select(x => x >= mean ? one : zero).ToArray();
+
+
+        }
+        public void BinaryLocalMean(int local)
+        {
+            //local jest rozmiare maski nie odleglosia od srodka
+            byte zero = 0, one = 1;
+
+            for (int x = 1; x < this.w - 1; x++)
+            {
+                for (int y = 1; y < this.h - 1; y++)
+                {
+                    var Suma = 0;
+                    for (int i = 0; i < local; i++)
+                    {
+                        for (int j = 0; j < local; j++)
+                        {
+                            Suma += this.Greycanal[x + i - 1][y + j - 1];
+
+                        }
+
+                    }
+                    var localmean = Suma / Math.Pow(local, 2);
+                    Greycanal[x][y] = Greycanal[x][y] >= localmean ? one : zero;
+                }
+            }
+        }
+        public void BinaryLocalGlobal(int local, int sigma)
+        {
+            byte zero = 0, one = 1;
+            var globalmean = utab.Sum(x => x) / utab.Length;
+            for (int x = 1; x < this.w - 1; x++)
+            {
+                for (int y = 1; y < this.h - 1; y++)
+                {
+                    var Suma = 0;
+                    for (int i = 0; i < local; i++)
+                    {
+                        for (int j = 0; j < local; j++)
+                        {
+                            Suma += this.Greycanal[x + i - 1][y + j - 1];
+
+                        }
+
+                    }
+                    var localmean = Suma / Math.Pow(local, 2);
+
+                    if ((globalmean - sigma < localmean) && (localmean < globalmean + sigma))
+                    {
+                        Greycanal[x][y] = Greycanal[x][y] >= localmean ? one : zero;
+                    }
+                    else
+                        //mozna dodac +/- sigma ale konieczny dodatkowy warunek(mysle ze lepsze z sigma)
+                        Greycanal[x][y] = Greycanal[x][y] >= globalmean? one : zero;
+                }
+            }
+        }
+        //binryzacja bez brzegów 
+        public void Bersen(int local)
+        {
+            byte zero = 0, one = 1;
+            var globalmean = utab.Sum(x => x) / utab.Length;
+            byte epsilon = 1;
+            var Suma = 0;
+            for (int x = 1; x < this.w - 1; x++)
+            {
+                for (int y = 1; y < this.h - 1; y++)
+                {
+                    Suma = 0;
+                    byte[] temp = new byte[local*local];
+                    for (int i = 0; i < local; i++)
+                    {
+                        
+                        for (int j = 0; j < local; j++)
+                        {
+                            temp[i]= this.Greycanal[x + i - 1][y + j - 1];
+
+                        }
+
+                    }
+
+                    var TempMax = temp.Max();
+                    var TempMin = temp.Min();
+                    var level=(TempMax+TempMin)/2;
+
+
+                    if ((TempMax-TempMax)<epsilon)
+                    {
+                        Greycanal[x][y] = Greycanal[x][y] >= globalmean ? one : zero;
+                    }
+                    else
+                        //mozna dodac +/- sigma ale konieczny dodatkowy warunek(mysle ze lepsze z sigma)
+                        Greycanal[x][y] = Greycanal[x][y] >= level ? one : zero;
+                }
+            }
+        }
+        public void Otsu()
+        {
+
+        }
+
+
+
+    }
+}
+
---
+++ lab01biometria/RGB.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01biometria
+{
+    class image_RGB : image_as_tab
+    {
+        public byte[][] B;
+        public byte[][] G;
+        public byte[][] R;
+        public byte[][] alfa;
+
+
+        public image_RGB(byte[] sourcePixels, int wight, int hight)
+            : base(sourcePixels, wight, hight)
+        {
+
+
+
+            int k = 0;
+            w = wight;
+            h = hight;
+
+            B = new byte[wight][];
+            G = new byte[wight][];
+            R = new byte[wight][];
+            alfa = new byte[wight][];
+            for (int i = 0; i < wight; i++)
+            {
+                B[i] = new byte[hight];
+                G[i] = new byte[hight];
+                R[i] = new byte[hight];
+                alfa[i] = new byte[hight];
+                for (int j = 0; j < hight; j++)
+                {
+
+                    k = 4 * (j * w + i);
+                    B[i][j] = sourcePixels[k];
+                    G[i][j] = sourcePixels[k + 1];
+                    R[i][j] = sourcePixels[k + 2];
+                    alfa[i][j] = sourcePixels[k + 3];
+
+                }
+            }
+        }
+
+        public override byte[] show()
+        {
+            return imagearray3Dto1D();
+        }
+        public override void negative()
+        {
+            base.negative();
+            int k = 0;
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+
+                    k = 4 * (j * w + i);
+                    B[i][j] = utab[k];
+                    G[i][j] = utab[k + 1];
+                    R[i][j] = utab[k + 2];
+                    alfa[i][j] = utab[k + 3];
+
+                }
+            }
+        }
+
+
+
+        public byte[] imagearray3Dto1D()
+        {
+
+            byte[] temp = new byte[w * h * 4];
+            for (int j = 0; j < h; j++)
+            {
+                for (int i = 0; i < w; i++)
+                {
+
+                    temp[4 * (j * w + i)] = B[i][j];
+                    temp[4 * (j * w + i) + 1] = G[i][j];
+                    temp[4 * (j * w + i) + 2] = R[i][j];
+                    temp[4 * (j * w + i) + 3] = alfa[i][j];
+
+
+                }
+            }
+            return temp;
+        }
+
+        public void normalize()
+        {
+            //max
+            var maxR = R.SelectMany(x => x).ToArray().Max();
+            var maxB = B.SelectMany(x => x).ToArray().Max();
+            var maxG = G.SelectMany(x => x).ToArray().Max();
+            //min
+            var minG = G.SelectMany(x => x).ToArray().Min();
+            var minB = B.SelectMany(x => x).ToArray().Min();
+            var minR = R.SelectMany(x => x).ToArray().Min();
+            var normalizeConstR = maxR - minR;
+            var normalizeConstG = maxR - minG;
+            var normalizeConstB = maxR - minB;
+
+
+            ///pixel[x,y]=255*(pixel[x,y]-minPix)/(maxPix-minPix) 
+
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    R[i][j] = (byte)(255 * (R[i][j] - minR) / normalizeConstR);
+                    G[i][j] = (byte)(255 * (G[i][j] - minG) / normalizeConstG);
+                    B[i][j] = (byte)(255 * (B[i][j] - minB) / normalizeConstB);
+                }
+            }
+
+
+
+
+
+
+        }
+        public image_Gray greyimage()
+        {
+            byte z;
+            byte[] temp = new byte[w * h * 4];
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    z = (byte)((this.B[i][j] + this.G[i][j] + this.R[i][j]) / 3);
+                    temp[4 * (j * w + i)] = z;
+                    temp[4 * (j * w + i) + 1] = z;
+                    temp[4 * (j * w + i) + 2] = z;
+                    temp[4 * (j * w + i) + 3] = alfa[i][j];
+                }
+
+            }
+            return new image_Gray(temp, w, h);
+
+        }
+
+        public image_Gray grey_naturalimage()
+        {
+            byte z;
+            byte[] temp = new byte[w * h * 4];
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    //0.3*R+0.59*G+0.11*B
+                    z = (byte)(0.11 * this.B[i][j] + 0.59 * this.G[i][j] + 0.3 * this.R[i][j]);
+                    temp[4 * (j * w + i)] = z;
+                    temp[4 * (j * w + i) + 1] = z;
+                    temp[4 * (j * w + i) + 2] = z;
+                    temp[4 * (j * w + i) + 3] = alfa[i][j];
+
+                }
+
+            }
+            return new image_Gray(temp, w, h);
+
+        }
+
+        public void sepia(byte Waga)
+        {
+
+
+            byte[] temp = new byte[w * h * 4];
+
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    //R=R+2*W, G=G+W, B=B
+
+                    if (G[i][j] + Waga <= 255)
+                        G[i][j] = (byte)(G[i][j] + Waga);
+                    else
+                        G[i][j] = 255;
+                    if (R[i][j] + 2 * Waga <= 255)
+                        R[i][j] = (byte)(R[i][j] + 2 * Waga);
+                    else
+                        R[i][j] = 255;
+
+                }
+
+            }
+
+
+        }
+        public void sobel()
+        {
+            Sobel Sobeltemp = new Sobel();
+            Sobeltemp.Convolve(this);
+        }
+
+        public image_RGB Roberts()
+        {
+            image_RGB temp = new image_RGB(this.utab, w, h);
+            for (int i = 0; i < w - 1; i++)
+            {
+                for (int j = 0; j < h - 1; j++)
+                {
+                    //|p(x,y) - p(x+1,y+1)|+|p(x+1,y)-p(x,y+1)|.
+                    temp.R[i][j] = (byte)(Math.Abs(R[i][j] - R[i + 1][j + 1]) + Math.Abs(R[i + 1][j] - R[i][j + 1]));
+                    temp.B[i][j] = (byte)(Math.Abs(B[i][j] - B[i + 1][j + 1]) + Math.Abs(B[i + 1][j] - B[i][j + 1]));
+                    temp.G[i][j] = (byte)(Math.Abs(G[i][j] - G[i + 1][j + 1]) + Math.Abs(G[i + 1][j] - G[i][j + 1]));
+
+
+                }
+
+
+            }
+            for (int j = 0; j < h; j++)
+            {
+                temp.R[w - 1][j] = temp.R[w - 2][j];
+                temp.B[w - 1][j] = temp.B[w - 2][j];
+                temp.G[w - 1][j] = temp.G[w - 2][j];
+            }
+            for (int j = 0; j < w; j++)
+            {
+                temp.R[j][h - 1] = temp.R[j][h - 2];
+                temp.B[j][h - 1] = temp.B[j][h - 2];
+                temp.G[j][h - 1] = temp.G[j][h - 2];
+            }
+            return temp;
+        }
+        public image_RGB histogram()
+        {
+            int[] Hist = new int[256];
+            int[] HistR = new int[256];
+            int[] HistG = new int[256];
+            int[] HistB = new int[256];
+            for (int k = 0; k < 256; k++)
+            {
+                HistR[k] = R.SelectMany(x => x).ToArray().Where((x, i) => i % 4 == 0 && x == k).ToArray().Length;
+                HistG[k] = G.SelectMany(x => x).ToArray().Where((x, i) => i % 4 == 0 && x == k).ToArray().Length;
+                HistB[k] = B.SelectMany(x => x).ToArray().Where((x, i) => i % 4 == 0 && x == k).ToArray().Length;
+            }
+            Hist = HistR.Select((x, i) => x = (x + HistG[i] + HistB[i]) / 3).ToArray();
+            int max = Hist.Max();
+            for (int i = 0; i < 256; i++)
+            {
+                Hist[i] = 180 * Hist[i] / max;
+            }
+
+            byte[][] histobraz = new byte[256][];
+            for (int i = 0; i < 256; i++)
+            {
+                histobraz[i] = new byte[180];
+
+                for (int j = 0; j < 180; j++)
+                {
+                    if (j < (180 - Hist[i]))
+                        histobraz[i][j] = 0;
+                    else
+                        histobraz[i][j] = 100;
+                }
+            }
+            byte[] temp = new byte[256 * 180 * 4];
+
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 180; j++)
+                {
+
+                    temp[4 * (j * 256 + i)] = histobraz[i][j];
+                    temp[4 * (j * 256 + i) + 1] = histobraz[i][j];
+                    temp[4 * (j * 256 + i) + 2] = histobraz[i][j];
+                    temp[4 * (j * 256 + i) + 3] = 255;
+
+
+                }
+            }
+
+            return new image_RGB(temp, 256, 180);
+            //byte[] histobraz = new byte[184320];
+
+
+            //for(int i=0;i<180;i++){
+
+            //    for (int e = 0; e < 256; e++)
+            //    {
+            //        if (i < 180 - Hist[e]){
+            //            histobraz[4*(i*256+e)] = 60;
+            //            histobraz[4*(i*256+e)+1] = 20;
+            //            histobraz[4*(i*256+e)+2] = 15;
+            //            histobraz[4*(i*256+e)+3] = 255; }
+
+
+            //        else{
+            //            histobraz[4*(i*256+e)] = 20;
+            //            histobraz[4*(i*256+e)+1] = 100;
+            //            histobraz[4*(i*256+e)+2] = 255;
+            //            histobraz[4*(i*256+e)+3] = 255;    
+            //    }}
+
+
+            //}
+            //return histobraz;
+        }
+
+
+
+
+    }
+}
